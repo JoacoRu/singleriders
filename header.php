596e6139fa62e3c7d5e0d395159ed84935ebe6d9
balas16k <joacorubek@gmail.com>
2018-08-28 16:00:19 -0300

    Header integrado

--- header.php
+++ header.php
@@ -11,7 +11,14 @@
 
 
 ?>
-
+  <html>
+  <head>
+    <link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Abel|Montserrat:400,400i,700,700i|Pacifico" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/header.css">
+  </head>
     <header>
       <nav class="navbar navbar-dark fixed-top">
         <a class="navbar-brand" href="index.php">
@@ -74,3 +81,4 @@
         <?php endif; ?>
       </nav>
     </header>
+  </html>
