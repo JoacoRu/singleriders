5994bc130a305ca2c9e514207fd63190c5544986
JoacoRu <joacorubek@gmail.com>
2018-09-08 15:59:58 -0300

    register en Objetos, tiene errores a solucionar, no suma id o valida si existe el mail

---
+++ class/prueba/index.php
@@ -0,0 +1,55 @@
+<?php 
+include_once('register.php');
+$errores = [];
+
+if($_POST)
+{
+    
+    $usuario = new Register();
+    $usuario->validarLogin();
+    if(empty($errores))
+    {
+        $usuario->guardarUsuario();
+    }
+    
+}
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+    <form method="post">
+        <input type="text" name="nombre" placeholder="Ingrese su nombre">
+        <br>
+        <p> <?php if (isset($errores['nombre'])): ?>
+            <?= $errores['nombre'] ?>
+            <?php endif; ?></p>
+        <input type="text" name="apellido" placeholder="Ingrese su apellido">
+        <br>
+        <p> <?php if (isset($errores['apellido'])): ?>
+            <?= $errores['apellido'] ?>
+            <?php endif; ?></p>
+        <input type="password" name="password" placeholder="****">
+        <br>
+        <p> <?php if (isset($errores['pass'])): ?>
+            <?= $errores['pass'] ?>
+            <?php endif; ?></p>
+        <input type="text" name="email" placeholder="Ingrese su email">
+        <br>
+        <p> <?php if (isset($errores['email'])): ?>
+            <?= $errores['email'] ?>
+            <?php endif; ?></p>
+        <button type="submit">Registrarse!</button>
+    </form>
+</body>
+</html>
