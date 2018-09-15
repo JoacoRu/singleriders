7153c56478fea24bae7dc1f30b5ed878e5d9af94
gabrielvallejos86 <gabrielvallejos86@hotmail.com>
2018-09-15 02:02:05 -0300

    correcion cards mis viajes y todos los viajes

---
+++ lateral_izquierdo.php
@@ -0,0 +1,18 @@
+
+  <div class="col-12 pt-4 col-md-4 col-lg-3 ml-lg-2 d-flex flex-column justify-content-flex-start align-items-center">
+    <div class="row w-100 pt-2">
+      <ul class="d-block list-unstyled w-100 py-4 border rounded shadow-perfil">
+        <li class="text-center"><img style="max-width: 150px;" class="border rounded-circle" src=".<?=$usuariologin['srcImagenperfil']?>" alt="" id="foto-perfil"></li>
+        <li class="text-center mt-3"><a class="btn btn-outline-secondary" href="editar_perfil.php"><img src="images/iconos/home/editar.png" alt=""> <span class="mb-1">Editar perfil</span></a></li>
+      </ul>
+    </div>
+    <div class="row justify-content-center">
+      <ul class="d-block list-unstyled mt-4 text-center p-3 border rounded">
+        <!--<li><a href="perfil.php">Ver mi Perfil</a></li>-->
+        <li><a href="crea2.php">Crear Viaje</a></li>
+        <li><a href="home.php">Ver mis Viajes</a></li>
+        <li><a href="viajes.php">Todos los Viajes</a></li>
+        <li><a href="mensajes.php">Ver mis Mensajes</a></li>
+      </ul>
+    </div>
+  </div>
--- mensajes.php
+++ mensajes.php
@@ -44,78 +44,82 @@
   <body>
     <?php require_once('header.php'); ?>
     <section class="mt-5">
-      <div class="container-fluid pt-5">
+      <div class="container-fluid pt-5 w-lg-75">
         <div class="row p-0 m-0 bg-white rounded home-row-main">
           <div class="col-12 p-0 top-muro-image d-flex align-items-center justify-content-center">
             <h1 class="font-weight-bold">Mensajes</h1>
           </div>
-          <div class="col-12 pt-3 col-md-3 d-flex justify-content-center">
-            <!--<ul class="list-unstyled">
-              <li>Mis viajes</li>
-              <li>Mensajes</li>
-              <li>Buscar destinos</li>
-            </ul>-->
-          </div>
-          <div class="col-12 p-md-0 col-md-6">
-            <h2 class="mt-4 mb-4">Enviar Mensaje</h2>
-            <div class="w-100 d-flex mt-4 pl-3 pr-3">
-              <div class="d-flex justify-content-center align-items-center border rounded-circle stepper-muro stepunomsj <?= !isset($_GET['userchat']) ? 'btn-primary' : '' ?>">
-                1
-              </div>
-              <div class="d-flex linea-steps" style="width:auto;height:1px;background:#cccccc">
-              </div>
-              <div class="d-flex justify-content-center align-items-center border rounded-circle stepper-muro stepdosmsj <?= isset($_GET['userchat']) ? 'btn-primary' : '' ?>">
-                2
-              </div>
-              <div class="d-flex linea-steps" style="width:auto;height:1px;background:#cccccc">
-              </div>
-              <div class="d-flex justify-content-center align-items-center border rounded-circle stepper-muro steptresmsj">
-                3
-              </div>
-            </div>
-            <div>
-              <form method="post" class="mt-4">
-                <div class="form-label-group" id="touser">
-                  <select id="to" name="to" class="form-control" onchange="$('#mensajearea').show();setactivo($('.stepdosmsj'));window.location.assign(window.location.origin+window.location.pathname+'?userchat='+document.getElementById('to').value)">
-                    <option value="elige_usuario">Seleccioná un usuario</option>
-                    <?php foreach ($allUsers as $nombre) { ?>
-                    <option <?= $nombre['id'] == $userchat ? 'selected' : '' ?> value="<?php echo $nombre['id']; ?>"><?php echo $nombre['email']; ?></option>
-                    <?php } ?>
-                  </select>
-                </div>
-                <div class="form-label-group" id="mensajearea" style="display:<?= isset($_GET['userchat']) ? 'block' : 'none' ?>">
-                  <textarea onkeyup="$('#enviar').show();setactivo($('.steptresmsj'));" class="form-control" name="mensaje" placeholder="Escribí tu mensaje..."></textarea>
-                </div>
-                <div class="container" id="enviar">
-                  <div class="row flex-column flex-md-row justify-content-md-between align-items-md-center">
-                    <button type="submit" class="btn btn-primary iniciar mb-3 mb-md-0">Enviar</button>
+          <div class="container-fluid">
+              <div class="row justify-content-center">
+                <?php require_once('lateral_izquierdo.php'); ?>
+                <div class="col-12 mt-2 col-md-8">
+                  <div class="container-fluid">
+                    <div class="row justify-content-center">
+                      <div class="col-12 col-lg-8">
+                        <h2 class="mt-4 mb-4 text-center">Enviar Mensaje</h2>
+                        <div class="w-100 d-flex mt-4 pl-3 pr-3">
+                          <div class="d-flex justify-content-center align-items-center border rounded-circle stepper-muro stepunomsj <?= !isset($_GET['userchat']) ? 'btn-primary' : '' ?>">
+                            1
+                          </div>
+                          <div class="d-flex linea-steps" style="width:auto;height:1px;background:#cccccc">
+                          </div>
+                          <div class="d-flex justify-content-center align-items-center border rounded-circle stepper-muro stepdosmsj <?= isset($_GET['userchat']) ? 'btn-primary' : '' ?>">
+                            2
+                          </div>
+                          <div class="d-flex linea-steps" style="width:auto;height:1px;background:#cccccc">
+                          </div>
+                          <div class="d-flex justify-content-center align-items-center border rounded-circle stepper-muro steptresmsj">
+                            3
+                          </div>
+                        </div>
+                        <div>
+                          <form method="post" class="mt-4">
+                            <div class="form-label-group" id="touser">
+                              <select id="to" name="to" class="form-control" onchange="$('#mensajearea').show();setactivo($('.stepdosmsj'));window.location.assign(window.location.origin+window.location.pathname+'?userchat='+document.getElementById('to').value)">
+                                <option value="elige_usuario">Seleccioná un usuario</option>
+                                <?php foreach ($allUsers as $nombre) { ?>
+                                <option <?= $nombre['id'] == $userchat ? 'selected' : '' ?> value="<?php echo $nombre['id']; ?>"><?php echo $nombre['email']; ?></option>
+                                <?php } ?>
+                              </select>
+                            </div>
+                            <div class="form-label-group" id="mensajearea" style="display:<?= isset($_GET['userchat']) ? 'block' : 'none' ?>">
+                              <textarea onkeyup="$('#enviar').show();setactivo($('.steptresmsj'));" class="form-control" name="mensaje" placeholder="Escribí tu mensaje..."></textarea>
+                            </div>
+                            <div class="container" id="enviar">
+                              <div class="row flex-column flex-md-row justify-content-md-between align-items-md-center">
+                                <button type="submit" class="btn btn-primary iniciar mb-3 mb-md-0">Enviar</button>
+                              </div>
+                              <div class="row mt-3">
+                              </div>
+                            </div>
+
+                          </form>
+                        </div>
+                        <div class="border rounded px-4 mb-3">
+                          <h2 class="mt-4 mb-4">Mensajes</h2>
+
+                          <?php foreach ($msj as $key) :?>
+                          <?php if (($key['to'] == $usuariologin['id'] && $key['from'] == $userchat) || ($key['from'] == $usuariologin['id'] && $key['to'] == $userchat)) :?>
+                            <p><strong><span class="<?= ( $key['from'] == $usuariologin['id'] ) ? 'userchatlogueado' : 'otrouserchat'  ?>"><?= $key['from'] == $usuariologin['id'] ? $usuariologin['nombre'].'</span> (el '.$key['fecha'] : $key['nombreRemitente'].'</span> (el '.$key['fecha']  ?>)</strong>: <?= $key['msj'] ?></p>
+                          <?php endif; ?>
+                          <?php endforeach; ?>
+
+                        </div>
+                      </div>
+                    </div>
                   </div>
-                  <div class="row mt-3">
-                  </div>
                 </div>
-
-              </form>
-            </div>
-            <div>
-              <h2 class="mt-4 mb-4">Mensajes</h2>
-
-              <?php foreach ($msj as $key) :?>
-              <?php if (($key['to'] == $usuariologin['id'] && $key['from'] == $userchat) || ($key['from'] == $usuariologin['id'] && $key['to'] == $userchat)) :?>
-                <p><strong><span class="<?= ( $key['from'] == $usuariologin['id'] ) ? 'userchatlogueado' : 'otrouserchat'  ?>"><?= $key['from'] == $usuariologin['id'] ? $usuariologin['nombre'].'</span> (el '.$key['fecha'] : $key['nombreRemitente'].'</span> (el '.$key['fecha']  ?>)</strong>: <?= $key['msj'] ?></p>
-              <?php endif; ?>
-              <?php endforeach; ?>
-
+                <div class="col-12 pt-3 col-md-3 d-flex align-items-center flex-column" style="display:none !important;">
+                  <!--<h5 class="mb-3">Top destinos</h5>
+                  <ul class="list-unstyled">
+                    <li>Europa del este</li>
+                    <li>Asia</li>
+                    <li>América</li>
+                  </ul>-->
+                </div>
+              </div>
             </div>
           </div>
-          <div class="col-12 pt-3 col-md-3 d-flex align-items-center flex-column">
-            <!--<h5 class="mb-3">Top destinos</h5>
-            <ul class="list-unstyled">
-              <li>Europa del este</li>
-              <li>Asia</li>
-              <li>América</li>
-            </ul>-->
-          </div>
-        </div>
       </div>
     </section>
 
