f4be70a67db81ba2134f95dd069024174ef9d3d7
balas16k <joacorubek@gmail.com>
2018-09-26 16:32:21 -0300

    home, posteo fix

--- home.php
+++ home.php
@@ -32,27 +32,48 @@
   
   <body>
     <?php require_once('header.php'); ?>
-    <section class="mt-5">
+    <section class="mt-5 section">
       <div class="container-fluid pt-5">
         <div class="row p-0 m-0 bg-white rounded home-row-main justify-content-center">
           <div class="col-12 p-0 top-muro-image d-flex align-items-center justify-content-center">
             <h1 class="font-weight-bold text-center">Muro</h1>
           </div>
-          <?php require_once('lateral_izquierdo.php'); ?>
+          <div class="lateral">
+            <?php require_once('lateral_izquierdo.php'); ?>
+          </div>
+      <!-- CREAR UN POST HTML -->
+
+     <!--  <article class="posteo_crear">
+        <div class="formulario_posteo">
+          <img style="max-width: 30px;" class="border rounded-circle" src=".<?=$usuariologin['srcImagenperfil']?>" alt="" id="foto-perfil">
+          <form method="get">
+            <textarea name="posteo"cols="30" rows="10" placeholder="¿Que estas pensado?"></textarea>
+            <button type="submit">Publicar</button>
+          </form>
         </div>
-      </div>
-          <article class="post">
-            <div class="posteos">
-              <div class="datos">
+      </article> -->
+
+      <!-- POSTEO HTML -->
+          <article class="articulo_post">
+            <div class="posteos_card">
+              <div class="datos_post">
                 <img style="max-width: 30px;" class="border rounded-circle" src=".<?=$usuariologin['srcImagenperfil']?>" alt="" id="foto-perfil">
                 <p><?= $usuariologin['nombre'] ?></p>
               </div>
               
-              <div class="contenido">
+              <div class="contenido_post">
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat esse tempora qui corrupti repellat quam at pariatur sapiente quia culpa, quasi dicta, et iste. Et, omnis veniam? Officia, asperiores culpa.</p>
+              </div>
+              <div class="post_interaccion">
+                <label for="me_gusta">Me gusta</label>
+                <img src="images/iconos/interaccion_posteo/me-gusta_no_seleccionado.png" alt="" name="me_gusta">
+                <label for="comentar">Comentar</label>
+                <img src="images/iconos/interaccion_posteo/comentario.png" alt="" name="comentar">
               </div>      
             </div>
           </article>
+          </div>
+      </div>
     </section>
 
     <?php require_once('footer.php'); ?>
