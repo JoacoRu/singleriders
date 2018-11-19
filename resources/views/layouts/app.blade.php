17d7eddec344def4b04ebc57dfa516a2d9e37c9e
JoacoRu <joacorubek@gmail.com>
2018-11-19 13:43:58 -0300

    validaciones js, falta crear las vistas para llamarlas

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,7 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/validacion.js') }}" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
@@ -76,5 +77,6 @@
             @yield('content')
         </main>
     </div>
+
 </body>
 </html>
