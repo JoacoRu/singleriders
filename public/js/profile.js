54a5b5c942a16097b68f22de862e0bde2e3f43aa
balas16k <joacorubek@gmail.com>
2018-11-23 11:57:41 -0300

    asdasd

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,12 +1,14 @@
 window.onload = function(){
-    let token = document.querySelector ('meta [name = "csrftoken”]'). GetAttribute ('content');
-    var form  = document.querySelector('form');
-    var post  = document.querySelector('textarea[name="posteo"]').value;
-    var url = 'http://127.0.0.1:8000/profileAjax';
-    var redirect = 'http://127.0.0.1:8000/home';
 
+    let token              = document.querySelector ('meta [name = "csrftoken"]');
+    var form               = document.querySelector('form');
+    var post               = document.querySelector('textarea[name="posteo"]').value;
+    var url                = 'http://127.0.0.1:8000/profile';
+    var redirect           = 'http://127.0.0.1:8000/home';
+    var postView           = document.querySelector('#publicaciones');
     var datosDelFormulario = new FormData();
-    datosDelFormulario.append('datos', JSON.stringify(form));
+        datosDelFormulario.append('datos', JSON.stringify(form));
+
    function sendData(){ 
     fetch(url, {
         headers: {
@@ -20,11 +22,13 @@ window.onload = function(){
         body: JSON.stringify({
           post: post,
         })
+
        })
         .then((data) => {
             form.reset();
             window.location.href = redirect;
         })
+
        .catch(function(error) {
            console.log(error);
          });
@@ -32,7 +36,29 @@ window.onload = function(){
 
     form.onsubmit = function(){
         sendData();
-
     }
 
+    function showPost(){
+        fetch("http://127.0.0.1:8000/profileAjaxGet")
+
+        .then(function (response) {
+            return response.json();
+            
+        })
+
+        .then(function (data) {
+            for(let indice in data){
+                let post =       "<div class='col-12 p-10 pt-4 col-md-8'><article class='articulo_post'><div class='posteos_card'><div class='datos_post'><img style='max-width: 30px;' class='border rounded-circle' src='imagen' alt=' id='foto-perfil'><p> {{ Auth::user()->name. ' '. Auth::user()->lastname}}</p></div><div class='contenido_post'><p>asdasdasdasd</p><input type='text' value='asdasdasds' hidden></div><div class='post_interaccion'><label for='me_gusta'>Me gusta</label><img src='images/iconos/interaccion_posteo/me-gusta_no_seleccionado.png' alt=' name='me_gusta'><label for='comentar'>Comentar</label><img src='images/iconos/interaccion_posteo/comentario.png' alt=' name='comentar'></div></div></article></div>";
+                postView.append(post);
+            };
+        })
+
+        .catch(function (error) {
+            console.log("The error was: " + error);
+        })
+    }
+
+    showPost()
+
+   
 }
