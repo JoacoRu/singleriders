c5ff901bce660e1fa4ac8f823bf52cee5c5efa83
Maybj <magomez21@gmail.com>
2018-11-26 12:13:05 -0300

    itinerario

--- app/Http/Controllers/FollowersController.php
+++ app/Http/Controllers/FollowersController.php
@@ -54,30 +54,23 @@ class FollowersController extends Controller
 
     }
 
-    public function showItinerary($travel_id){
+    /* public function showItinerary($travel_id){
         $date1=Follower::find('dateIn');
         $date2=Follower::find('dateOut');
         $diff=$date2->diffInDays($date1);
         dd($diff);
-        /* return view ('sharedTravel'['sharedTRavel' => $diff]) */
+        /* return view ('sharedTravel'['sharedTRavel' => $diff]) */ 
+
+        public function showItinerary($travel_id){
+        $date1= Carbon::parse($request->input('dateIn'));
+        $date2= Carbon::parse($request->input('dateOut'));
+        $diff = $date2->diffInDays($dateIn);
+        dd($diff);
+        return view ('/sharedTravel', ['sharedTravel' => $diff]);
+        }
     }
+    
 
 
-    /**<p><?=$date1->format('d-m-y') ?></p>
-<p><?=$date2->format('d-m-y') ?></p>
-<p><?=$diff->format('La resta da %d días') ?></p>
+  
 
-<?php for ($i=0; $i < $diff->format('%d') ; $i++) :?>
-  <p>Día <?= $i ?></p>
-  <p><div class="input-group">
-    <span class="input-group-addon"></span>
-    <input type="text" class="form-control" placeholder="Itinerario">
-  </div></p>
-<?php endfor;?> */
-    /* public function showItinerary(){
-        $travel=Follower::
-        $date1=Find::(dateIn)
-        $date2=
-        $diff=$date2-$date1;
-    } */
-}
